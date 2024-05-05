5f66b3955a922c1ca5f24f89913c7ed9d6c72633
priyanka.m3 <priyanka.m3@glowtouch.com>
2024-05-05 22:45:14 +0530

    Project initialisation

---
+++ DailyTodos/DailyTodos/DailyTodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTodosApp.swift
+//  DailyTodos
+//
+//  Created by Priyanka Mathur on 05/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyTodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
