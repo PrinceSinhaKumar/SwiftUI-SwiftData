f67ab4defe3adf7b27be26b015367ec30aa85057
Prince Shrivastava <prince.kumar042@gmail.com>
2024-08-07 23:48:23 +0530

    DownloadImage: Using Combine Change: 1. Refactor and update DownloadImageManager 2. Add getImageWithCombine to download image using combine method of urlsession

--- Concurrency-Swift/ConcurrencySwift/DownloadImageWithAsyncAwait/DownloadImageWithAsyncAwait.swift
+++ Concurrency-Swift/ConcurrencySwift/DownloadImageWithAsyncAwait/DownloadImageWithAsyncAwait.swift
@@ -6,20 +6,36 @@
 //
 
 import SwiftUI
+import Combine
 
 class DownloadImageManager {
     
-    func getImage(completionHandler: @escaping (UIImage?, Error?) -> ()) {
+    fileprivate func responseHandler(_ data: Data?,
+                                     _ response: URLResponse?) -> UIImage? {
+        guard let imageData = data,
+              let image = UIImage(data: imageData),
+              let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 else {
+            return nil
+        }
+        return image
+    }
+    
+    func getImageWithEscapingClouser(completionHandler: @escaping (UIImage?, Error?) -> ()) {
         let url = URL(string: "https://picsum.photos/200")!
-        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            guard let imageData = data,
-                  let image = UIImage(data: imageData),
-                  let httpResponse = response as? HTTPURLResponse,
-                  httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 else {
-                return completionHandler(nil, error)
-            }
+        URLSession.shared.dataTask(with: URLRequest(url: url)) { [weak self] data, response, error in
+            let image = self?.responseHandler(data,
+                                        response)
             return completionHandler(image, nil)
         }.resume()
+    }
+    
+    func getImageWithCombine() -> AnyPublisher<UIImage?, Error> {
+        let url = URL(string: "https://picsum.photos/200")!
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(responseHandler)
+            .mapError({ $0 })
+            .eraseToAnyPublisher()
     }
 }
 
@@ -27,14 +43,29 @@ class DownloadImageWithAsyncAwaitViewModel: ObservableObject {
     
     @Published var image: UIImage?
     let imageManager = DownloadImageManager()
-    
+    var cancelable = Set<AnyCancellable>()
     func fetchImage() {
-        imageManager.getImage { [weak self] image, error in
+       /* imageManager.getImageWithEscapingClouser { [weak self] image, error in
             if let image {
                 self?.image = image
             }
             print(error?.localizedDescription ?? "")
-        }
+        } */
+        
+        imageManager.getImageWithCombine()
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error)
+                default:
+                    break
+                }
+            } receiveValue: { [weak self] image in
+                self?.image = image
+            }
+            .store(in: &cancelable)
+
     }
 }
 
